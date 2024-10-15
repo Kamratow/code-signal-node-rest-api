f8b3d1698a3b488cd0df697798a362ac799afc31
Kamil Ratowski <kamilratowski90@gmail.com>
2024-10-15 12:09:32 +0200

    Added simple handlers for 404 and 500 errors

--- index.js
+++ index.js
@@ -7,6 +7,17 @@ app.get("/", (_req, res) => {
   res.send("This has been returned from server!");
 });
 
+// handle 404 not found error
+app.use((_req, res) => {
+  res.status(404).send("Resource not found");
+});
+
+// handle 500 internal server error
+app.use((err, _req, res, next) => {
+  console.log(err.stack);
+  res.status(500).send("Internal server error - try again later");
+});
+
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
